f51d2bca7c5289978b9aecbf48d97f63360b5ad8
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-29 14:31:24 +0200

    etape 7.1 ajout map vide avec Vue2Leaflet

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,10 +1,35 @@
 <template>
-    <h1>Carte des fournisseurs</h1>
+    <div style="height: 80vh">
+        <h1>Carte des fournisseurs</h1>
+        <LMap :zoom="zoom" :center="center">
+            <LTileLayer :url="url"></LTileLayer>
+            <LMarker :lat-lng="[47.413220, -1.219482]"></LMarker>
+            <LMarker :lat-lng="[46.193220, 4.82]"></LMarker>
+            <LMarker :lat-lng="[45.193220, 6.82]"></LMarker>
+            <LMarker :lat-lng="[47.03220, -0.9482]"></LMarker>
+            <LMarker :lat-lng="[46.03220, 2.9482]"></LMarker>
+        </LMap>
+    </div>
+
 </template>
 
 <script>
+    import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
     export default {
         name: "SuppliersMap",
+        components: {
+            LMap,
+            LTileLayer,
+            LMarker
+        },
+        data() {
+            return {
+                url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
+                zoom: 6,
+                center: [46.5322, 2.9482],
+                bounds: null
+            };
+        }
 
     }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import App from './App.vue'
 import HelloWorld from "./components/HelloWorld";
 import SuppliersList from "./components/SuppliersList";
 import SuppliersMap from "./components/SuppliersMap";
 import Supplier from "./components/Supplier";
+
+// Import the Icon
+import { Icon } from "leaflet";
+// Import the whole Leaflet CSS
+import "leaflet/dist/leaflet.css";
+
+Vue.config.productionTip = false;
+
+// Resolve an issue where the markers would not appear
+delete Icon.Default.prototype._getIconUrl;
+
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
 Vue.use(VueRouter);
 
 const router = new VueRouter ({
@@ -18,9 +35,9 @@ const router = new VueRouter ({
 });
 
 
-import App from './App.vue'
 
-Vue.config.productionTip = false;
+
+
 
 new Vue({
   router,
