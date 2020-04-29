e7a6626f310f343a775eae502db6543045fb6974
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-29 11:08:40 +0200

    etape 5.1 vue Supplier ok

--- src/components/Supplier.vue
+++ src/components/Supplier.vue
@@ -1,8 +1,20 @@
 <template>
     <div>
-        <h1>Fournisseur </h1>
-        <h3>A du stock? </h3>
-        <h5>Date du dernier relevé : </h5>
+        <h1> Fournisseur: {{name}} </h1>
+        <h3  v-if ="status" style="color:green;"> A du stock? OK </h3>
+        <h3 v-else style="color:red;"> A du stock? KO</h3>
+        <h5>Date du dernier relevé :
+            {{checkedAt.toLocaleString({
+                weekday:'short',
+                month: 'short',
+                day:'numeric',
+                year: 'numeric',
+                hour:'2-digit',
+                minute:'2-digit',
+                second:'2-digit',
+                timeZone: 'UTC',
+                timeZoneName:'short'})}}
+        </h5>
     </div>
 
 
@@ -15,8 +27,7 @@
             return {
                 name: 'I <3 Algues',
                 status: false, // est ce qu'il y a du stock
-                checkedAt: new Date() // date de la dernière mise à jour du stock
-            }
+                checkedAt: new Date() /* date de la dernière mise à jour du stock*/}
         }
     }
 </script>
