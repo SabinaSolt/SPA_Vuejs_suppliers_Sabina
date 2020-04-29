fc75b4e7d95ede726665d671f425bde3c4ac0df5
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-04-29 15:30:59 +0200

    etape 7.3 ajout marqueurs sur la carte

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -3,11 +3,10 @@
         <h1>Carte des fournisseurs</h1>
         <LMap :zoom="zoom" :center="center">
             <LTileLayer :url="url"></LTileLayer>
-            <LMarker :lat-lng="[47.413220, -1.219482]"></LMarker>
-            <LMarker :lat-lng="[46.193220, 4.82]"></LMarker>
-            <LMarker :lat-lng="[45.193220, 6.82]"></LMarker>
-            <LMarker :lat-lng="[47.03220, -0.9482]"></LMarker>
-            <LMarker :lat-lng="[46.03220, 2.9482]"></LMarker>
+            <LMarker v-for="supplier in suppliers"
+                     :key ="supplier.id"
+                     :lat-lng="[supplier.latitude, supplier.longitude]"></LMarker>
+
         </LMap>
     </div>
 
@@ -27,10 +26,21 @@
                 url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
                 zoom: 6,
                 center: [46.5322, 2.9482],
-                bounds: null
+                bounds: null,
+                suppliers: [
+                    {
+                        id: 1,
+                        latitude: 45,
+                        longitude: 3
+                    },
+                    {
+                        id: 2,
+                        latitude: 46,
+                        longitude: 5
+                    }
+                ]
             };
-        }
-
+        },
     }
 </script>
 
